e781da6f9ab0758a423dd5a72ace9158dbaf18c1
Shreeya Bhetwal <bhetwalshreeya@gmail.com>
2020-02-08 13:25:54 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
 const express = require('express')
-const axios = require('axios')
-
 const app = express()
-const port = 3000
+const port = 3000;
+const path = require('path');
+const axios = require('axios');
 
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json());
-app.use(express.static('public'));
-
-
-app.get('/seed', (req, res) => {
-  axios.get('http://localhost:3003/seed')
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      console.log(err)
-    })
-});
 
 app.get('/houses', (req, res) => {
-  axios.get('http://localhost:3004/houses')
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  axios.get('http://18.144.88.205:3004/houses')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.get('/api/summary/data/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://3.17.76.247:3002/api/summary/data/${id}`)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.get('/seed', (req, res) => {
+  axios.get('http://18.223.108.8:3003/seed')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 });
 
 app.get('/listings', (req, res) => {
-  axios.get('http://localhost:3001/listings')
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      console.log(err)
-    })
-});
-
-
-
-app.get('/api/data/:id', (req, res) =>{
-  const { id } = req.params;
-  axios.get(`http://localhost:3002/api/data/${id}`)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  axios.get('http://13.57.29.35:3001/listings')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 });
 
 
